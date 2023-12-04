8e1bbc361296c17b804af12b1aa0e0521f51ce78
denniswynn22 <147005838+denniswynn22@users.noreply.github.com>
2023-12-04 15:10:35 -0500

    Full project w/ presentation notes

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // 1. press one number - we save the number
 // 2. press the operator - save the operator
 // 3. press second number - save second number 
-// 4. press Equal sign - make calulation
+// 4. press Equal sign - make calculation
 
-// Variables
+// Setting the Variables
 let firstNumber
 let secondNumber
+let decimal = document.querySelector('.decimal-btn') // important discuss for presentation
 let step = 0
-let operation
+let operation = null
 let result = 0
 
 let numArray = []
@@ -16,7 +17,8 @@ let secondNumArray = []
 let display = document.getElementById('display')
 
 // Function to call requirements into the calculator.= - numbers and operators
-function getNumber(num) {
+function getNumber(num){
+console.log (numArray.join(""))
     if(step === 0 || step === 1){ 
         // truthy / falsy operator: if true then keep step 0 if false then get step 1 which is to to take the number from array, make a string, then a number
         numArray.push(num) // [1,3,5,7]
@@ -31,11 +33,28 @@ function getNumber(num) {
     }
 }
 
-function getOperator(op){
-    step = 2
-    operation = op
+// Decimal notes for presentation
+
+function handleDecimal() {
+    console.log("i just pressed the decimal button")
+    console.log(display.value)
+    if (display.value = undefined) {screen.textContent = 0.}
+    if (!display.value.includes('.')) { // what does that Bang do doe
+        display.value += '.';
+    }
 }
-// Function to clear the display back to 0 when necessary
+
+// PMVP feature - two consecutive operators will give message b/c my operators are connected*
+function getOperator(op) {
+    if(operation === null){
+    step = 2
+    operation = op // operation function is called by "op"
+    } else {
+        display.value = "Hi Friend" 
+    }
+}
+
+// Function to clear the display back to 0 / null values when necessary
 function clearDisplay(){
    display.value = 0
    firstNumber = null
@@ -47,7 +66,11 @@ function clearDisplay(){
    secondNumArray = []
 }
 
+
+// Calculation functions
 const calculateEquals = () =>{
+    console.log(firstNumber, secondNumber)
+
     if(operation === '+') {
         result = firstNumber + secondNumber
         display.value = result
@@ -60,8 +83,5 @@ const calculateEquals = () =>{
     } else if (operation === '/'){
         result = firstNumber / secondNumber
         display.value = result
-    } else if (operation === '-,+,*,/'){
-        result = 'Hi Dennis'
-        display.value = result
-    }
+    } 
 }
